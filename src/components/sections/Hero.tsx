b14ae68dfa91f0750835c1f271232a7cf2834eba
anderson camero <cameroanderson22@gmail.com>
2025-07-17 03:28:11 -0500

    styles are added to the hero, more image

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,6 +1,7 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { motion } from "motion/react";
 import { CardCarousel } from "../UI/CardCarousel";
+import { AnimatedWord } from "../UI/AnimateWord";
 // import { Button } from "../UI/Button";
 
 const heroImages = [
@@ -22,11 +23,10 @@ const heroImages = [
 ];
 
 export const Hero: React.FC = () => {
-  const heroRef = useRef<HTMLDivElement>(null);
   const [isVisible, setIsVisible] = useState<boolean>(false);
   const [isPaused, setIsPaused] = useState<boolean>(false);
 
-  const CaruselImages = [...heroImages, ...heroImages];
+  const CaruselImages = [...heroImages, ...heroImages, ...heroImages];
 
   useEffect(() => {
     document.fonts.ready.then(() => {
@@ -43,7 +43,7 @@ export const Hero: React.FC = () => {
           animate={isVisible ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
           transition={{
             duration: 0.6,
-            delay: index * 0.1,
+            delay: 0.6,
             ease: [0.25, 0.46, 0.45, 0.94],
           }}
           className="inline-block mr-2"
@@ -55,106 +55,102 @@ export const Hero: React.FC = () => {
     [isVisible]
   );
 
-  const splitTextByChars = useCallback(
-    (text: string) => {
-      return text.split("").map((char, index) => (
-        <motion.span
-          key={index}
-          initial={{ opacity: 0, scale: 0.8 }}
-          animate={
-            isVisible ? { opacity: 1, scale: 1 } : { opacity: 0, scale: 0.8 }
-          }
-          transition={{
-            duration: 0.4,
-            delay: 0.5 + index * 0.05,
-            ease: "easeOut",
-          }}
-          className="inline-block"
-        >
-          {char}
-        </motion.span>
-      ));
-    },
-    [isVisible]
-  );
-
   return (
-    <section
-      ref={heroRef}
-      className="pt-32 sm:pt-40 pb-16 bg-gradient-to-br from-[var(--color-primary)]/15 via-[var(--color-light)] to-[var(--color-light)]"
-    >
-      <div className="container mx-auto px-4">
-        <div className="flex flex-col items-center text-center max-w-4xl mx-auto">
-          <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-[var(--color-dark)] mb-6 leading-tight">
-            {splitText("En cada voz habita una")}
-            <span className="block sm:inline text-transparent bg-clip-text bg-gradient-to-r from-[var(--color-primary)] to-[var(--color-secondary)]">
-              {" "}
-              {splitTextByChars("historia")}
-            </span>
-          </h1>
+    <section className="pt-32 sm:pt-40 pb-16 bg-gradient-to-br from-[var(--color-primary)]/15 via-[var(--color-light)] to-[var(--color-light)]">
+      <div className="w-full mx-auto px-4 ">
+        <div className="flex flex-col lg:flex-row items-center lg:items-start text-left w-full gap-8">
+          <div className="flex-1 space-y-6">
+            <motion.h1 className="font-bold text-3xl sm:text-4xl lg:text-5xl text-[var(--color-dark)] leading-tight flex flex-wrap gap-3">
+              {splitText("En cada")}
+              <AnimatedWord
+                colorClass="bg-[var(--color-secondary)]/30"
+                delay={0.6}
+              >
+                voz
+              </AnimatedWord>
+              {splitText("habita una")}
+              <AnimatedWord
+                colorClass="bg-[var(--color-yellow)]/30 "
+                delay={0.6}
+              >
+                historia
+              </AnimatedWord>
+              {splitText("que merece ser")}
+              <AnimatedWord
+                colorClass="bg-[var(--color-purple)]/30"
+                delay={0.6}
+              >
+                escuchada
+              </AnimatedWord>
+              {splitText("y")}
+              <AnimatedWord
+                colorClass="bg-[var(--color-orange)]/30"
+                delay={0.6}
+              >
+                escuchada
+              </AnimatedWord>
+            </motion.h1>
 
-          <motion.p
-            className="text-base sm:text-lg md:text-xl text-[var(--color-gray)] mb-8 leading-relaxed px-4"
-            initial={{ opacity: 0, y: 30 }}
-            animate={isVisible ? { opacity: 1, y: 0 } : { opacity: 0, y: 30 }}
-            transition={{
-              duration: 0.8,
-              delay: 1.5,
-              ease: "easeOut",
-            }}
-          >
-            Nuestro compromiso es contribuir a la construcción de un mundo en el
-            que cada persona pueda encontrar su lugar a través de una educación
-            transformadora.
-          </motion.p>
+            <motion.p
+              className="text-base sm:text-lg md:text-xl text-[var(--color-gray)] leading-relaxed"
+              initial={{ opacity: 0, y: 30 }}
+              animate={isVisible ? { opacity: 1, y: 0 } : { opacity: 0, y: 30 }}
+              transition={{
+                duration: 0.8,
+                delay: 1.5,
+                ease: "easeOut",
+              }}
+            >
+              Nuestro compromiso es contribuir a la construcción de un mundo en
+              el que cada persona pueda encontrar su lugar a través de una
+              educación transformadora.
+            </motion.p>
+          </div>
 
-          <motion.div
-            className="w-full"
-            initial={{ opacity: 0, scale: 0.9 }}
-            animate={
-              isVisible ? { opacity: 1, scale: 1 } : { opacity: 0, scale: 0.9 }
-            }
-            transition={{ type: "spring", duration: 0.8, delay: 0.5 }}
-          >
-            <div className="my-8 sm:my-12">
-              <div className="overflow-hidden w-full">
-                <div
-                  className={`flex whitespace-nowrap ${
-                    isPaused ? "animate-scroll-paused" : "animate-scroll"
-                  }`}
-                  onMouseEnter={() => setIsPaused(true)}
-                  onMouseLeave={() => setIsPaused(false)}
-                >
-                  {CaruselImages.map((img, index) => (
-                    <CardCarousel
-                      imageSrc={img.src}
-                      altText={img.alt}
-                      titleImage={img.title}
-                      key={index}
-                    />
-                  ))}
-                </div>
+          <div className="w-full max-w-xs sm:max-w-sm md:max-w-sm lg:max-w-xs xl:max-w-sm mx-auto lg:mx-0">
+            <motion.img
+              src="/imgs/ninos-en-circulo.png"
+              alt="Niños acostados en círculo"
+              className="w-full h-auto rounded-xl shadow-md object-cover"
+              initial={{ opacity: 0, scale: 0.95 }}
+              animate={
+                isVisible
+                  ? { opacity: 1, scale: 1 }
+                  : { opacity: 0, scale: 0.95 }
+              }
+              transition={{ type: "spring", duration: 0.8, delay: 0.8 }}
+            />
+          </div>
+        </div>
+        <motion.div
+          className="w-full"
+          initial={{ opacity: 0, scale: 0.9 }}
+          animate={
+            isVisible ? { opacity: 1, scale: 1 } : { opacity: 0, scale: 0.9 }
+          }
+          transition={{ type: "spring", duration: 0.8, delay: 0.5 }}
+        >
+          <div className="my-8 sm:my-12">
+            <div className="overflow-hidden w-full">
+              <div
+                className={`flex whitespace-nowrap ${
+                  isPaused ? "animate-scroll-paused" : "animate-scroll"
+                }`}
+                onMouseEnter={() => setIsPaused(true)}
+                onMouseLeave={() => setIsPaused(false)}
+              >
+                {CaruselImages.map((img, index) => (
+                  <CardCarousel
+                    imageSrc={img.src}
+                    altText={img.alt}
+                    titleImage={img.title}
+                    key={index}
+                  />
+                ))}
               </div>
             </div>
-          </motion.div>
-
-          {/* <div className="flex flex-col sm:flex-row gap-4 justify-center mt-12">
-            <Button
-              className="bg-[var(--color-primary)] hover:bg-[var(--color-secondary)] text-white rounded-full px-8 py-6 text-lg"
-              onClick={() => setActiveTab("que-ofrecemos")}
-            >
-              Descubre nuestros servicios
-              <ArrowRight className="ml-2 w-5 h-5" />
-            </Button>
-            <Button
-              className="border-[var(--color-primary)] text-[var(--color-primary)] hover:bg-[var(--color-light)] rounded-full px-8 py-6 text-lg bg-transparent"
-              onClick={() => setActiveTab("quienes-somos")}
-            >
-              <Play className="mr-2 w-5 h-5" />
-              Conoce nuestra historia
-            </Button>
-          </div> */}
-        </div>
+          </div>
+        </motion.div>
       </div>
     </section>
   );
--- src/components/sections/Stats.tsx
+++ src/components/sections/Stats.tsx
@@ -3,7 +3,7 @@ import type React from "react";
 export const Stats: React.FC = () => {
   return (
     <section className="py-1.5 sm:py-2 lg:py-2.5 bg-[var(--color-light)] mt-2 sm:mt-2.5">
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="w-full mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center mb-8 sm:mb-12 lg:mb-16">
           <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-[var(--color-dark)] leading-tight">
             El impacto de
@@ -16,29 +16,29 @@ export const Stats: React.FC = () => {
         </div>
       </div>
 
-      <div className="flex flex-col sm:flex-row flex-wrap justify-center items-center gap-6 sm:gap-8 lg:gap-12 max-w-6xl mx-auto">
-        <div className="flex flex-col items-center text-center group flex-1 min-w-[140px] max-w-[200px]">
+      <div className="flex flex-wrap justify-center items-start w-full max-w-7xl mx-auto">
+        <div className="flex flex-col items-center text-center group w-full sm:w-1/2 lg:w-1/4 px-4">
           <div className="inline-block bg-[var(--color-primary)]/30 text-[var(--color-dark)] font-bold text-3xl sm:text-4xl lg:text-5xl px-4 sm:px-6 py-2 sm:py-3 rounded-lg rotate-[-2deg] mb-3 sm:mb-4 transition-transform duration-300 hover:rotate-0 hover:scale-105">
             500+
           </div>
           <div className="text-[var(--color-gray)]">Educadores formados</div>
         </div>
 
-        <div className="lex flex-col items-center text-center group flex-1 min-w-[140px] max-w-[200px]">
+        <div className="flex flex-col items-center text-center group w-full sm:w-1/2 lg:w-1/4 px-4">
           <div className="inline-block bg-[var(--color-primary)]/30 text-[var(--color-dark)] font-bold text-3xl sm:text-4xl lg:text-5xl px-4 sm:px-6 py-2 sm:py-3 rounded-lg rotate-[-2deg] mb-3 sm:mb-4 transition-transform duration-300 hover:rotate-0 hover:scale-105">
             50+
           </div>
           <div className="text-[var(--color-gray)]">Instituciones</div>
         </div>
 
-        <div className="lex flex-col items-center text-center group flex-1 min-w-[140px] max-w-[200px]">
+        <div className="flex flex-col items-center text-center group w-full sm:w-1/2 lg:w-1/4 px-4">
           <div className="inline-block bg-[var(--color-primary)]/30 text-[var(--color-dark)] font-bold text-3xl sm:text-4xl lg:text-5xl px-4 sm:px-6 py-2 sm:py-3 rounded-lg rotate-[-2deg] mb-3 sm:mb-4 transition-transform duration-300 hover:rotate-0 hover:scale-105">
             15+
           </div>
           <div className="text-[var(--color-gray)]">Años de experiencia</div>
         </div>
 
-        <div className="lex flex-col items-center text-center group flex-1 min-w-[140px] max-w-[200px]">
+        <div className="flex flex-col items-center text-center group w-full sm:w-1/2 lg:w-1/4 px-4">
           <div className="inline-block bg-[var(--color-primary)]/30 text-[var(--color-dark)] font-bold text-3xl sm:text-4xl lg:text-5xl px-4 sm:px-6 py-2 sm:py-3 rounded-lg rotate-[-2deg] mb-3 sm:mb-4 transition-transform duration-300 hover:rotate-0 hover:scale-105">
             98%
           </div>
