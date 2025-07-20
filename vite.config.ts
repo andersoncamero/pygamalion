c353ce3224262fc30b92da51a3a7deaedd008647
anderson camero <cameroanderson22@gmail.com>
2025-07-20 17:37:59 -0500

    configurations for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindss()],
   server: {
-    hmr: false, // Desactiva Hot Module Replacement
+    hmr: true, // Desactiva Hot Module Replacement
     watch: {
       usePolling: false, // Desactiva el polling de archivos
-    }
-  }
-})
+      ignored: ["**/node_modules/**", "**/public/imgs/optimized/**"],
+    },
+    headers: {
+      "Cache-Control": "public, max-age=0",
+    },
+  },
+});
